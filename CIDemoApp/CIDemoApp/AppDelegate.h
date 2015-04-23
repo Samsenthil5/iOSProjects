3cb89a52816433bbb65f451b68829bc987439433
Samsenthil5 <samsenthil5@gmail.com>
2015-04-22 22:36:45 -0400

    added new project

---
+++ CIDemoApp/CIDemoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CIDemoApp
+//
+//  Created by SHREE SAHANA on 4/22/15.
+//  Copyright (c) 2015 SHREE SAHANA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CIDemoApp/CIDemoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CIDemoApp
+//
+//  Created by SHREE SAHANA on 4/22/15.
+//  Copyright (c) 2015 SHREE SAHANA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
